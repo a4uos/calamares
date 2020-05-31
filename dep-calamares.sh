95cbd6a74c65015e60b258f1aabf4c8189930e3c
ordanax <ordanax@ya.ru>
2020-06-01 03:20:32 +0500

    Add files via upload

---
+++ dep-calamares.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# kconfig
+# kcoreaddons
+# ki18n
+# solid
+# qt5-svg
+# gptfdisk
+# rsync
+# python
+# squashfs-tools
+# qt5-webengine
+# git
+sudo pacman -S yaml-cpp
+sudo pacman -S parted
+sudo pacman -S boost
+sudo pacman -S hwinfo
+sudo pacman -S polkit-qt5
+sudo pacman -S kdebase-runtime
+sudo pacman -S dmidecode
+sudo pacman -S kpmcore
+sudo pacman -S qt5ct
+sudo pacman -S qt5-styleplugins-git
+sudo pacman -S kparts
+sudo pacman -S extra-cmake-modules
+sudo pacman -S qt5-tools
